4eb613e7559ce0d30ab7a34b13f39ad3e5e4c896
Michel Perez <m1ch3lp3r3z@gmail.com>
2018-01-09 13:56:35 -0700

    Adds vendor helper methods in service class

--- src/Service/AccountService.php
+++ src/Service/AccountService.php
@@ -2,7 +2,9 @@
 
 namespace Mr\Sdk\Service;
 
+use Mr\Sdk\Model\Account\OAuthToken;
 use Mr\Sdk\Model\Account\User;
+use Mr\Sdk\Model\Account\Vendor;
 use Mr\Sdk\Repository\Account\OAuthTokenRepository;
 use Mr\Sdk\Repository\Account\UserRepository;
 use Mr\Sdk\Repository\Account\VendorRepository;
@@ -20,7 +22,7 @@ class AccountService extends BaseService
      * Returns user by id
      *
      * @param $id
-     * @return mixed
+     * @return User
      */
     public function getUser($id)
     {
@@ -42,11 +44,44 @@ class AccountService extends BaseService
      * Returns first user matching filters
      *
      * @param array $filters
-     * @return mixed
+     * @return User
      */
     public function findOneUser(array $filters = [])
     {
         return $this->_get(UserRepository::class)->findOne($filters);
+    }
+
+    /**
+     * Returns user by id
+     *
+     * @param $id
+     * @return Vendor
+     */
+    public function getVendor($id)
+    {
+        return $this->_get(VendorRepository::class)->get($id);
+    }
+
+    /**
+     * Returns all vendors matching filters
+     *
+     * @param array $filters
+     * @return array
+     */
+    public function findVendors(array $filters = [])
+    {
+        return $this->_get(VendorRepository::class)->find($filters);
+    }
+
+    /**
+     * Returns first vendor matching filters
+     *
+     * @param array $filters
+     * @return Vendor
+     */
+    public function findOneVendor(array $filters = [])
+    {
+        return $this->_get(VendorRepository::class)->findOne($filters);
     }
 
     /**
@@ -60,6 +95,10 @@ class AccountService extends BaseService
         return $this->_get(UserRepository::class)->create($data);
     }
 
+    /**
+     * @param array $data
+     * @return Vendor
+     */
     public function createVendor(array $data = [])
     {
         return $this->_get(VendorRepository::class)->create($data);
@@ -70,6 +109,10 @@ class AccountService extends BaseService
         return $this->_get(UserRepository::class)->get($id);
     }
 
+    /**
+     * @param $provider
+     * @return OAuthToken
+     */
     public function getOAuthTokenByProvider($provider)
     {
         return $this->_get(OAuthTokenRepository::class)->getByProvider($provider);
